8f133201d7d525bea6de54d789c5099cfffd6109
Juan Gallego <fbdavid37@utp.edu.co>
2017-10-08 22:55:11 -0500

    Hash1parte

--- src/cap3/mapas/TablaHash.java
+++ src/cap3/mapas/TablaHash.java
@@ -3,12 +3,42 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package estructurasdedatos.cap3.mapas;
+package cap3.mapas;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
 
 /**
  *
- * @author tusk
+ * @author David
  */
 public class TablaHash {
-    
+
+    String[] arreglo;
+    int tama;
+    int contador;
+
+    public TablaHash(int tam) {
+        tama = tam;
+        arreglo = new String[tam];
+        Arrays.fill(arreglo, "-1");
+
+    }
+
+    public void miFuncionHash(String[] cadenaArreglo, String[] arreglo) {
+        int i;
+        for (i = 0; i < cadenaArreglo.length; i++) {
+            String elemento = cadenaArreglo[i];
+            int indiceArreglo = Integer.parseInt(elemento) % 7;
+            System.out.println("El indice es" + indiceArreglo + "para el valor" + elemento);
+            while (arreglo[indiceArreglo] != "-1") {
+                indiceArreglo++;
+                System.out.println("Hay una colision en el indice" + (indiceArreglo- 1) 
+                        + "cambiar a " + indiceArreglo);
+                indiceArreglo%=tama;
+            }
+            arreglo[indiceArreglo]=elemento;
+        }
+    }
+
 }
